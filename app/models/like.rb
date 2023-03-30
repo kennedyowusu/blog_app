300eb1a755297977f794e01c3b3f1f3ccfe4193d
Kennedy Owusu <kennediowusu@gmail.com>
2023-03-30 11:08:55 +0000

    update the foreign key in the posts table from author to author_id

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
   belongs_to :post, counter_cache: true
 
   validates :user, :post, presence: true
