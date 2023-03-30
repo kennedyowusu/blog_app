e09777dec2f8d131ecd44e843bfcbbb9a9cc0c27
Kennedy Owusu <kennediowusu@gmail.com>
2023-03-30 11:09:32 +0000

    resolve linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,21 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments, foreign_key: "post_id", dependent: :destroy
-  has_many :likes, foreign_key: "post_id", dependent: :destroy
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
 
   validates :title, presence: true
   validates :text, presence: true, length: { minimum: 10 }
 
   def increment_comments_counter
-    self.increment!(:comments_counter)
+    increment!(:comments_counter)
   end
 
   def increment_likes_counter
-    self.increment!(:likes_counter)
+    increment!(:likes_counter)
   end
 
   def decrement_likes_counter
-    self.decrement!(:likes_counter)
+    decrement!(:likes_counter)
   end
 
   def increment_posts_counter
