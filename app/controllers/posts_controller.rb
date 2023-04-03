9d96587a02edd7a0bcc3285b96c107e1572e1c5b
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-03 19:39:54 +0000

    create controllers, routes, and views for posts and users

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  def index
+    # fetch all posts belonging to current user
+  end
+
+  def show
+    # fetch single post using params[:id]
+  end
+
+  def specific_user
+    # fetch post belonging to current user using params[:id]
+  end
+end
