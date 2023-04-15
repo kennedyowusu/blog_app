66c459d69bf99dae88c7ce84e2eabb599dbd4a37
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-15 09:20:19 +0000

    update the like model code

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post
-  after_save :update_post_likes_counter
+  belongs_to :post, counter_cache: true
+  after_commit :update_post_likes_counter, on: [:create, :destroy]
 
   private
 
   def update_post_likes_counter
-    post.update(likes_counter: post.likes.count)
+    post.update_column(:likes_counter, post.likes_count)
   end
 end
