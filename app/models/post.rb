21033c0d27bacdec91394b5f02395f75bcbbb159
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-15 09:16:20 +0000

    optimize the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,24 +9,25 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def comments_counter
-    comments.count
+    comments.size
   end
 
   def likes_counter
-    likes.count
+    likes.size
   end
 
   def add_comment(comment)
     comments << comment
+    update(comments_counter: comments_counter + 1)
   end
 
-  def recent_comments()
+  def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
   private
 
   def update_author_posts_counter
-    author.update(posts_counter: author.posts.count)
+    author.update(posts_counter: author.posts.size)
   end
 end
