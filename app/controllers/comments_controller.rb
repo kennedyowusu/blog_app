b66ad9ace44f34f9703eb2e87221ca279fc2b798
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-15 08:55:05 +0000

    refactor the comment controller code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :set_user_and_post, only: [:new, :create]
+
   def new
-    @user = current_user
-    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
   def create
+    @comment = @post.comments.new(author: @user, post: @post, text: comment_params[:text])
+    if @comment.save
+      redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user_and_post
     @user = current_user
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(author: @user, post: @post, text: params[:comment][:text])
-    @comment.save
-    redirect_to user_post_path(@post.author, @post)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
