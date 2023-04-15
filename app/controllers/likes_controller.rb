3b00760174598a0315dd34f35ed902ceb0147503
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-15 08:56:45 +0000

    optimize the like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikesController < ApplicationController
   def create
     @user = current_user
-    @post = Post.find(params[:post_id])
-    @like = Like.new(author: @user, post: @post)
-    @like.save
-    redirect_to user_post_path(@user, @post)
+    @post = Post.find_by(id: params[:post_id])
+    @like = @post.likes.build(author: @user)
+    @like.save!
+    redirect_back(fallback_location: root_path)
   end
 end
