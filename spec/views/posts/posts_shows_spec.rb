02b71691951d1abd726cdde7dbe6036010846eed
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-21 10:47:09 +0000

    solve linter error

--- spec/views/posts/posts_shows_spec.rb
+++ spec/views/posts/posts_shows_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'root page features' do
         bio: 'Teacher from Poland.', posts_counter: 0
       )
     ]
-​
+    ​
     @posts = [
       @post1 = Post.create(
         author: @user1,
@@ -31,41 +31,41 @@ RSpec.describe 'root page features' do
         likes_counter: 0
       )
     ]
-​
+    ​
     visit user_post_path(@users.first, @posts.first)
   end
-​
+  ​
   describe '#PostShowPage' do
     it 'Should display post title' do
       expect(page).to have_content(@post1.title)
     end
-​
+
     it 'should display the post author' do
       expect(page).to have_content(@posts.first.author.name)
     end
-​
+
     it 'should display the number of comments' do
       expect(page).to have_content(@posts.first.comments_counter)
     end
-​    it 'should display the number of likes' do
-    expect(page).to have_content(@posts.first.likes_counter)
-  end
-​
-  it 'should display the post body' do
-    expect(page).to have_content(@posts.first.text)
-  end
-​
-  it 'should display the name of the commentor' do
-    @posts.first.comments.each do |comment|
-      expect(page).to have_content(comment.author.name)
+
+    it 'should display the number of likes' do
+      expect(page).to have_content(@posts.first.likes_counter)
     end
-  end
-​
-  it 'should display each comment left by the commenter' do
-    @posts.first.comments.each do |comment|
-      expect(page).to have_content(comment.body)
+
+    it 'should display the post body' do
+      expect(page).to have_content(@posts.first.text)
     end
-  endt
-   
+
+    it 'should display the name of the commentor' do
+      @posts.first.comments.each do |comment|
+        expect(page).to have_content(comment.author.name)
+      end
+    end
+
+    it 'should display each comment left by the commenter' do
+      @posts.first.comments.each do |comment|
+        expect(page).to have_content(comment.body)
+      end
+    end
   end
 end
